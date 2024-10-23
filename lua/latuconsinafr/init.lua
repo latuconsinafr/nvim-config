7052a91a1f31c61fedb11f17043772df2747461e
Farista Latuconsina <faristalatuconsina@gmail.com>
2024-10-23 18:32:25 +0700

    set the basic configuration

---
+++ lua/latuconsinafr/init.lua
@@ -0,0 +1,4 @@
+-- Load personal configurations
+require("latuconsinafr.packer")  -- Plugin management
+require("latuconsinafr.set")     -- Custom settings
+require("latuconsinafr.remap")   -- Key mappings
---
+++ lua/latuconsinafr/set.lua
@@ -0,0 +1,33 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- Enable smart indentation
+vim.opt.smartindent = true
+
+-- Enable clipboard access
+vim.opt.clipboard = 'unnamedplus'
+
+-- Search settings
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- UI enhancements
+vim.opt.termguicolors = true
+
+-- Scrolling and UI
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- Performance
+vim.opt.updatetime = 50
+
+-- Leader key
+vim.g.mapleader = " "
