6ab52001905cf8a4d65fb69b97acc91075e75fbc
Farista Latuconsina <faristalatuconsina@gmail.com>
2025-06-10 08:13:58 +0700

    feat: add go to preview

--- lua/latuconsinafr/plugins/nvim-lspconfig.lua
+++ lua/latuconsinafr/plugins/nvim-lspconfig.lua
@@ -155,23 +155,28 @@ return {
       local on_attach = function(_, bufnr)
         local opts = { buffer = bufnr }
 
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration", unpack(opts) })
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition", unpack(opts) })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation", unpack(opts) })
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation", unpack(opts) })
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature help", unpack(opts) })
+
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder,
+          { desc = "Add workspace folder", unpack(opts) })
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder,
+          { desc = "Remove workspace folder", unpack(opts) })
         vim.keymap.set("n", "<leader>wl", function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        end, { desc = "List workspace folders", unpack(opts) })
+
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Go to type definition", unpack(opts) })
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol", unpack(opts) })
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action", unpack(opts) })
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find references", unpack(opts) })
+
         vim.keymap.set("n", "<leader>f", function()
           vim.lsp.buf.format({ async = true })
-        end, opts)
+        end, { desc = "Format buffer", unpack(opts) })
       end
 
       -- Explicit setup for `lua_ls` to register the vim global
