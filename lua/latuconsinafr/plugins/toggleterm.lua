37f1286540bf40d2fb45acef6a144fe3cb888f1e
Farista Latuconsina <faristalatuconsina@gmail.com>
2025-08-31 13:13:09 +0700

    fix: fix toggleterm display

--- lua/latuconsinafr/plugins/toggleterm.lua
+++ lua/latuconsinafr/plugins/toggleterm.lua
@@ -13,22 +13,38 @@ return {
       end,
       hide_numbers = true,
       shade_filetypes = {},
-      autochdir = true,       -- Automatically change to the directory of the active buffer
-      shade_terminals = true, -- Apply a shading effect to the terminal
-      shading_factor = 2,     -- Degree of shading (1-3)
-      start_in_insert = true, -- Start terminal in insert mode
+      autochdir = true,        -- Automatically change to the directory of the active buffer
+      shade_terminals = true,  -- Apply a shading effect to the terminal
+      shading_factor = 2,      -- Degree of shading (1-3)
+      start_in_insert = true,  -- Start terminal in insert mode
       insert_mappings = false, -- Disabled to use custom toggle system
-      persist_size = true,    -- Retain terminal size across sessions
-      persist_mode = true,    -- if set to true (default) the previous terminal mode will be remembered
-      direction = 'float',    -- Choose between 'horizontal', 'vertical', 'tab', or 'float'
+      persist_size = true,     -- Retain terminal size across sessions
+      persist_mode = true,     -- if set to true (default) the previous terminal mode will be remembered
+      direction = 'float',     -- Choose between 'horizontal', 'vertical', 'tab', or 'float'
       float_opts = {
         width = math.floor(vim.o.columns * 0.85),
         height = math.floor(vim.o.lines * 0.75),
         title_pos = 'center',
+        border = 'curved',  -- Add border for better visibility
+        winblend = 0,       -- No transparency issues
       },
       close_on_exit = true, -- Close the terminal when the process exits
       shell = vim.o.shell,
       auto_scroll = true,
+      terminal_mappings = true,                               -- Apply terminal keymaps
+      env = {
+        COLUMNS = tostring(math.floor(vim.o.columns * 0.85)), -- Set proper column width
+      },
+      on_create = function(term)
+        -- Set proper terminal options when created
+        vim.cmd("setlocal wrap")            -- Enable text wrapping
+        vim.cmd("setlocal linebreak")       -- Break at word boundaries
+        vim.cmd("setlocal nolist")          -- Don't show whitespace chars
+        vim.cmd("setlocal textwidth=0")     -- No hard text width limit
+        vim.cmd("setlocal wrapmargin=0")    -- No wrap margin
+        vim.cmd("setlocal sidescroll=0")    -- Don't scroll horizontally
+        vim.cmd("setlocal sidescrolloff=0") -- No side scroll offset
+      end,
     })
 
     local Terminal = require('toggleterm.terminal').Terminal
@@ -51,14 +67,14 @@ return {
 
     -- Keymaps for terminal management
     -- Main toggle (replaces Ctrl+\)
-    vim.keymap.set({'n', 't'}, '<C-\\>', '<cmd>lua toggle_terminal(1)<CR>', { desc = "Toggle Alpha" })
+    vim.keymap.set({ 'n', 't' }, '<C-\\>', '<cmd>lua toggle_terminal(1)<CR>', { desc = "Toggle Alpha" })
 
     -- Individual terminal toggles
-    vim.keymap.set({'n', 't'}, '\\1', '<cmd>lua toggle_terminal(1)<CR>', { desc = "Toggle Alpha" })
-    vim.keymap.set({'n', 't'}, '\\2', '<cmd>lua toggle_terminal(2)<CR>', { desc = "Toggle Beta" })
-    vim.keymap.set({'n', 't'}, '\\3', '<cmd>lua toggle_terminal(3)<CR>', { desc = "Toggle Gamma" })
-    vim.keymap.set({'n', 't'}, '\\4', '<cmd>lua toggle_terminal(4)<CR>', { desc = "Toggle Delta" })
-    vim.keymap.set({'n', 't'}, '\\5', '<cmd>lua toggle_terminal(5)<CR>', { desc = "Toggle Epsilon" })
+    vim.keymap.set({ 'n', 't' }, '\\1', '<cmd>lua toggle_terminal(1)<CR>', { desc = "Toggle Alpha" })
+    vim.keymap.set({ 'n', 't' }, '\\2', '<cmd>lua toggle_terminal(2)<CR>', { desc = "Toggle Beta" })
+    vim.keymap.set({ 'n', 't' }, '\\3', '<cmd>lua toggle_terminal(3)<CR>', { desc = "Toggle Gamma" })
+    vim.keymap.set({ 'n', 't' }, '\\4', '<cmd>lua toggle_terminal(4)<CR>', { desc = "Toggle Delta" })
+    vim.keymap.set({ 'n', 't' }, '\\5', '<cmd>lua toggle_terminal(5)<CR>', { desc = "Toggle Epsilon" })
 
     -- Terminal mode mappings
     function _G.set_terminal_keymaps()
